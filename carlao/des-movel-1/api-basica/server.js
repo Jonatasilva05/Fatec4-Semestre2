4fb52b561cb48494cf61acc94d1d85633b90501d
jonatasmoraes05@gmail.com <0220832311013@fatectq.local>
2025-04-01 08:59:16 -0300

    01-04-25

--- carlao/des-movel-1/api-basica/server.js
+++ carlao/des-movel-1/api-basica/server.js
@@ -7,6 +7,16 @@ app.get("/", (request, response) => {
     response.send({ message: "servidor rodando ..."});
 })
 
+app.get("/login", (request, response) => {
+    const user = "admin";
+    const senha = "123";
+    if ( user === "admin" && senha === "123" ) {
+        response.send({ message: "Login efetuado com sucesso"});
+    } else {
+        response.send({ message: "Usuario ou senha invalidos"});
+    }
+})
+
 app.listen(3000, () => {
     console.log("servidor rodando na porta: 3000");
 })
